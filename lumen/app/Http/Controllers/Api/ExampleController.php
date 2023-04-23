76d9e40228ec0904d5281da31a1a00490898a0eb
Alejandro Toga <aotech10@gmail.com>
2023-04-23 15:09:20 -0600

    added lumen && dockerfiles

---
+++ lumen/app/Http/Controllers/Api/ExampleController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+class ExampleController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    //
+}
